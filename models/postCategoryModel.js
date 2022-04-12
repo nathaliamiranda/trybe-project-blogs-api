e20ecfd5f2efad0080b32d546808e15b675cb20d
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-11 23:16:58 -0300

    Fix: postsCategory  model changes

--- models/postCategoryModel.js
+++ models/postCategoryModel.js
@@ -7,14 +7,14 @@ module.exports = (sequelize, _DataTypes) => {
     models.Category.belongsToMany(models.BlogPost, {
       as: 'blogposts',
       through: PostCategory,
-      foreignKey: 'postId',
-      otherKey: 'categoryId',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
     });
     models.BlogPost.belongsToMany(models.Category, {
       as: 'categories',
       through: PostCategory,
-      foreignKey: 'categoryId',
-      otherKey: 'postId',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
     });
   };
   return PostCategory;
