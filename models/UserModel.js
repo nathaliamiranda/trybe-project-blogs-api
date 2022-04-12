6c4fec8f9aec08f7e7e06e613fbbd0a2e9a14ce9
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-11 23:31:17 -0300

    Fix: User  model changes

--- models/UserModel.js
+++ models/UserModel.js
@@ -37,7 +37,7 @@ module.exports = (sequelize) => {
     });
 
     User.associate = (models) => {
-      User.hasMany(models.BlogPost, { foreignKey: 'postId', as: 'posts' });
+      User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'user' });
     };
   return User;
 };
